b2d7f65c15c0bf63f1581dc1fd1c00b76bf7f019
spencer <spencerwilliamspalding@gmail.com>
2026-02-01 13:36:37 -0500

    working on rx module

--- uart.sv
+++ uart.sv
@@ -35,23 +35,29 @@ module uart_tx #(
 (
   input logic clk,
   input logic rst,
-  input logic baud_tick,
+  input logic baud_tick_r,
   input logic [7:0] data,
   input logic data_in_valid,
   output logic data_out,
-  output logic data_in_ready
-)
+  output logic data_in_ready,
+  output logic baudclk_en_n
+);
 
   typedef enum logic [1:0] { 
     IDLE,
     START,
-    TX
+    TX,
+    XXX = 'x
 
   } state_t;
 
   state_t state_r;
+  
+  logic [3:0] cntr;
 
   logic [7:0] data_r;
+  
+  logic edge_reg;
 
   always_ff @(posedge clk or posedge rst) begin
 
@@ -60,8 +66,129 @@ module uart_tx #(
       state_r <= IDLE;
       data_out <= 1'b1;
       data_in_ready <= 1'b0;
+      baudclk_en_n <= 1'b1;
+      edge_reg <= '0;
+      cntr <= '0;
 
     end else begin
+
+    edge_reg <= baud_tick_r;
+
+
+    case (state_r)
+    
+      IDLE: begin
+
+        if (data_in_valid) begin 
+
+          data_in_ready <= 1'b0;
+
+          data_r <= data;
+
+          state_r <= START;
+
+          baudclk_en_n <= 1'b0;
+          
+        
+        end else begin
+
+          data_out <= 1'b1;
+
+          data_in_ready <=1'b1;
+
+          baudclk_en_n <= 1'b1;
+
+        end
+
+      end
+
+      START: begin
+
+          if(baud_tick_r > edge_reg) begin 
+            -
+            state_r <= TX;
+            data_out <= 1'b0;
+
+          end
+        end
+      
+
+
+      TX: begin
+
+        if(baud_tick_r > edge_reg) begin
+
+          data_out <= data_r[0];
+
+          data_r <= data_r >> 1;
+
+          cntr <= cntr + 1'b1;
+
+            if(cntr == 4'b1000) begin 
+              state_r <= IDLE;
+              data_out <= 1'b1;
+              cntr <= '0;
+            end
+        end
+        
+
+      end
+
+
+    endcase
+
+  end
+end
+
+endmodule
+
+
+
+module uart_rx #(
+  parameter int baudrate = 115200,
+  parameter int clkHz = 100_000_000   
+)
+(
+  input logic clk,
+  input logic rst,
+  input logic baud_tick_r,
+  output logic [7:0] data,
+  output logic data_out,
+  input logic data_in_ready,
+  input logic baudclk_en_n
+);
+
+  typedef enum logic [1:0] { 
+    IDLE,
+    START,
+    RX,
+    XXX = 'x
+
+  } state_t;
+
+    state_t state_r;
+  
+  logic [3:0] cntr;
+
+  logic [7:0] data_r;
+  
+  logic edge_reg;
+
+  always_ff @(posedge clk or posedge rst) begin
+
+    if(rst) begin
+
+      state_r <= IDLE;
+      data_out <= 1'b1;
+      data_in_ready <= 1'b0;
+      baudclk_en_n <= 1'b1;
+      edge_reg <= '0;
+      cntr <= '0;
+
+    end else begin
+
+    edge_reg <= baud_tick_r;
+
 
     case (state_r)
     
@@ -70,10 +197,10 @@ module uart_tx #(
       end
 
       START: begin
-      
-      end
 
-      TX: begin
+        end
+      
+      RX: begin
 
       end
 
@@ -81,5 +208,6 @@ module uart_tx #(
     endcase
 
   end
+end
 
 endmodule
--- uart_tb.sv
+++ uart_tb.sv
@@ -6,12 +6,33 @@ module uart_tick_gen_tb;
     logic rst;
     logic uart_tick;
 
+    logic baudclk_en_n;
+    logic [7:0] data;
+    logic data_in_valid;
+    logic data_out;
+    logic data_in_ready;
 
-    baud_tick DUT (
+
+
+
+    baud_tick DUT_1 (
         .clk(clk),
-        .rst(rst),
+        .rst(baudclk_en_n),
         .baud_clk(uart_tick)
     );
+
+    uart_tx DUT_2 (
+        .clk(clk),
+        .rst(rst),
+        .baud_tick_r(uart_tick),
+        .data(data),
+        .data_out_valid(data_in_valid),
+        .data_out(data_out),
+        .data_out_ready(data_in_ready),
+        .baudclk_en_n(baudclk_en_n)
+    );
+
+
 
     initial begin
         clk = 0;
@@ -20,7 +41,12 @@ module uart_tick_gen_tb;
 
     initial begin
         rst <= 1'b1;
+        data <= 8'b10101010;
+        data_out_valid <= 1'b1;
         #10 rst <= 1'b0;
+        // #12 data_in_valid <= 1'b0;
+
+        
     end
 
 
